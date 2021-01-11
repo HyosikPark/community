e3889a3f6aeefdca8e7e0532ec65e87a5360e328
hyogooz <gytlr01@naver.com>
2021-01-11 19:53:07 +0900

    's'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,9 @@ import '../styles/category.scss';
 import '../styles/board.scss';
 import '../styles/write.scss';
 import '../styles/post.scss';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 import { ApolloProvider } from '@apollo/client';
 import Layout from '../components/Layout';
