e9d92c7730690eb5e963e425e04e917de43ef943
Hyosik Park <hyosik.dev@gmail.com>
2021-10-20 14:06:39 +0900

    add getserversideProps

--- pages/category.tsx
+++ pages/category.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import React, { ChangeEvent, useCallback, useState } from 'react';
-import { menu, Menu } from '../util/Menu';
+import Head from "next/head";
+import React, { ChangeEvent, useCallback, useState } from "react";
+import { menu, Menu } from "../util/Menu";
 
 interface CategoryProps {
   menu: Menu[];
@@ -13,7 +13,7 @@ export async function getStaticProps() {
 }
 
 function Category({ menu }: CategoryProps) {
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
   const findStar = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
@@ -39,36 +39,40 @@ function Category({ menu }: CategoryProps) {
       <Head>
         <title>Categories - biaskpop Forum</title>
         <meta
-          name='description'
-          content='BTS BlackPink TWICE NCT SEVENTEEN EXO The Boyz GOT7 IZONE GI-DLE Oh My Girl SHINEE...'
+          name="description"
+          content="BTS BlackPink TWICE NCT SEVENTEEN EXO The Boyz GOT7 IZONE GI-DLE Oh My Girl SHINEE..."
         />
       </Head>
-      <div className='category_container'>
-        <div className='category_head'>
-          <label htmlFor='search'>
+      <div className="category_container">
+        <div className="category_head">
+          <label htmlFor="search">
             <input
-              type='text'
-              id='search'
-              name='search'
-              placeholder='Search...'
+              type="text"
+              id="search"
+              name="search"
+              placeholder="Search..."
               value={search}
               onChange={findStar}
             />
           </label>
         </div>
-        <section className='category'>
+        <section className="category">
           {menu.map((list) => (
             <ul key={list.initial} className={list.initial}>
               <div
                 className={`${list.initial.slice(0, 1)} category_title`}
               >{`${list.initial.slice(0, 1).toUpperCase()} `}</div>
-              <div className='artist_list'>{category(list)}</div>
+              <div className="artist_list">{category(list)}</div>
             </ul>
           ))}
         </section>
       </div>
     </>
   );
+}
+
+export async function getServerSideProps(context: any) {
+  return { props: null };
 }
 
 export default Category;
